5e74d806c82b16b0f30175fc66d708ab1e3be844
sverkercaron-sys <sverkercaron@gmail.com>
2026-03-03 11:58:54 +0100

    Add ToastProvider and ToastViewport exports to toast.tsx

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -6,6 +6,11 @@ import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
+const ToastProvider = ToastPrimitives.Provider
+
+const ToastViewport = ToastPrimitives.Viewport
+
+
 const toastVariants = cva(
   "group pointer-events-auto relative w-full items-center justify-between space-x-4 overflow-hidden rounded-md border border-gray-200 p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full flex bg-white",
   {
@@ -112,6 +117,8 @@ export type ToasterToast = Toast & {
 
 export {
   type ToastActionElement,
+    ToastProvider,
+    ToastViewport,
   Toast,
   ToastAction,
   ToastClose,
